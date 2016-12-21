86d4bdaee85514c618c7d02a8f594b796144d6fd
Vladislav Abramov <abramov.vlad182@gmail.com>
2016-12-21 20:41:53 +0300

    Added pyLabs

--- python/labs/py-lab-8.py
+++ python/labs/py-lab-8.py
@@ -1,7 +1,5 @@
-from itertools import chain
-
 
 def simple_numbers(n):
-    return [i for i in chain([2], xrange(3, n, 2)) if all([i % d for d in xrange(2, i)])]
+    return [i for i in xrange(1, n, 2) if all([i % d for d in xrange(2, i)])]
 
 print simple_numbers(30)
